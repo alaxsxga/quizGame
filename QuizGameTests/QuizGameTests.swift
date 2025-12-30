2a9535fdc0c94c5a47cbaee07a793d80c2b2e0d8
Eddev <alaxsxga.dev@gmail.com>
2025-12-30 16:34:12 +0800

    Initial Commit

---
+++ QuizGameTests/QuizGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuizGameTests.swift
+//  QuizGameTests
+//
+//  Created by Ed Liao on 2025/12/30.
+//
+
+import Testing
+
+struct QuizGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
